18fa377532cb815f00b1fe67eb5de59d04a7b303
nimeshpanchal <92348124+panchalnimesh@users.noreply.github.com>
2023-04-06 13:06:20 +0800

    add output for ecs task definition (#12)

--- modules/service/outputs.tf
+++ modules/service/outputs.tf
@@ -7,3 +7,7 @@ output "ecs_service_name" {
   description = "The name of the ECS service"
   value       = try(aws_ecs_service.this.name, "")
 }
+output "aws_ecs_task_definition" {
+  description = "ARN of the ECS service"
+  value       = try(aws_ecs_task_definition.this.arn, "")
+}
