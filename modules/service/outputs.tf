69ae8eb5e217821d7c3eecabbee83d26bda6805e
panchalnimesh <npanchal@sph.com.sg>
2023-04-06 12:55:20 +0800

    cosmatic change

--- modules/service/outputs.tf
+++ modules/service/outputs.tf
@@ -8,12 +8,12 @@ output "ecs_service_name" {
   value       = try(aws_ecs_service.this.name, "")
 }
 
-output "aws_ecs_task_definition" {
+output "aws_ecs_task_definition_name" {
   description = "The name of the ECS service"
   value       = try(aws_ecs_task_definition.this.name, "")
 }
 
-output "aws_ecs_task_definition" {
+output "aws_ecs_task_definition_arn" {
   description = "ARN of the ECS service"
   value       = try(aws_ecs_task_definition.this.arn, "")
 }
