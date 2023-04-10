d0fdf83c944961a9b8ef5db79553fa111bdd6e14
panchalnimesh <npanchal@sph.com.sg>
2023-04-10 18:05:07 +0800

    cosmatic change

--- outputs.tf
+++ outputs.tf
@@ -5,30 +5,30 @@ output "ecs_cluster_kms_arn" {
 
 output "ecs_cluster_id" {
   description = "ID of the ECS Cluster"
-  value       = try(aws_ecs_cluster.this.id, "")
+  value       = try(module.cluster.ecs_cluster_id, "")
 }
 
 output "ecs_cluster_arn" {
   description = "ARN of the ECS Cluster"
-  value       = try(aws_ecs_cluster.this.arn, "")
+  value       = try(module.cluster.ecs_cluster_arn, "")
 }
 
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
-  value       = try(aws_ecs_cluster.this.name, "")
+  value       = try(module.cluster.ecs_cluster_name, "")
 }
 
 output "ecs_service_arn" {
   description = "ARN of the ECS service"
-  value       = try(aws_ecs_service.this.id, "")
+  value       = try(module.cluster.ecs_service_arn, "")
 }
 
 output "ecs_service_name" {
   description = "The name of the ECS service"
-  value       = try(aws_ecs_service.this.name, "")
+  value       = try(module.cluster.ecs_service_name, "")
 }
 
 output "aws_ecs_task_definition" {
   description = "ARN of the ECS service"
-  value       = try(aws_ecs_task_definition.this.arn, "")
+  value       = try(module.cluster.aws_ecs_task_definition, "")
 }
